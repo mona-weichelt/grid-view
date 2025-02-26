4728a8ed560f73e566215ec5e83b1abdf15b5b22
Mona Weichelt <morgana.weichelt@gmail.com>
2025-02-26 16:35:45 +0100

    added basic search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,42 @@
 "use client";
 
-import GridView from "@/components/GridView";
+import GridView, { GridItem } from "@/components/GridView";
 import data from "../../data/grid-items.json";
+import { useState } from "react";
+
+//TODO: include regex
+const search = (term: string, data: GridItem[]): GridItem[] =>
+  data.filter((item) => {
+    return (
+      item.description.includes(term) ||
+      item.title.includes(term) ||
+      item.imagePath.includes(term)
+    );
+  });
 
 export default function Home() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const filteredData = search(searchTerm, data);
+  const isSearchActive = searchTerm.length > 0;
+  const hasResults = filteredData.length > 0;
+  const isSearchFailed = isSearchActive && !hasResults;
+
   return (
     <div className="min-h-screen flex flex-col font-[family-name:var(--font-geist-sans)]">
       <header className="sticky top-0 bg-white p-2 flex flex-row justify-between items-center shadow-md">
         <h1 className="hidden sm:block">Mona's Grid View</h1>
         <input
           placeholder="Search for grid items..."
+          onChange={(e) => setSearchTerm(e.target.value)}
           className="p-2 flex-1 rounded-md border hover:border-blue-500"
         />
       </header>
       <main className="flex-1">
-        <GridView data={data} firstPageSize={2} pageSize={1} />
+        {isSearchFailed ? (
+          <h1>Sorry :c</h1>
+        ) : (
+          <GridView data={filteredData} firstPageSize={2} pageSize={1} />
+        )}
       </main>
       <footer className="p-8 text-center bg-pink-300">
         <p>Copyright © 2025 Mona Weichelt</p>
--- src/components/GridView.tsx
+++ src/components/GridView.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { useState } from "react";
 
-type GridItem = {
+export type GridItem = {
   title: string;
   description: string;
   imagePath: string;
