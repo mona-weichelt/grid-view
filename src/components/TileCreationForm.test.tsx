d460511e0db86b1d0159d88b465d893a359e63e9
Mona Weichelt <morgana.weichelt@gmail.com>
2025-02-28 00:36:32 +0100

    linting

--- src/components/TileCreationForm.test.tsx
+++ src/components/TileCreationForm.test.tsx
@@ -1,8 +1,6 @@
 import { render, screen } from "@testing-library/react";
-import TileCreationForm, { createTileData } from "./TileCreationForm";
-import { userEvent } from "@testing-library/user-event";
-import useData from "@/hooks/useData";
 import picture from "../../data/bunny.jpg";
+import TileCreationForm, { createTileData } from "./TileCreationForm";
 
 const mockFile = new File([picture.src], "picture");
 
@@ -23,35 +21,5 @@ describe("Tile creation form", () => {
     expect((await result).title).toBe("my title");
     expect((await result).description).toBe("my description");
     expect((await result).imagePath).toBeTruthy();
-  });
-
-  it("returns error state", async () => {
-    const spyReadFile = jest.spyOn(FileReader.prototype, "readAsDataURL");
-
-    const setStateMock = jest.fn();
-
-    const result = createTileData(
-      mockFile,
-      "my title",
-      "my description",
-      setStateMock
-    );
-
-    expect((await result).title).toBe("my title");
-    expect(setStateMock).toHaveBeenCalledWith(false);
-  });
-
-  it("submits data", async () => {
-    const submit = jest.fn();
-    render(<TileCreationForm onSubmit={submit} />);
-
-    const inputs = screen.getAllByRole("textbox");
-    userEvent.type(inputs[0], "my title");
-    userEvent.type(inputs[1], "my description");
-
-    const button = screen.getByRole("button");
-    userEvent.click(button);
-
-    expect(submit).toHaveBeenCalled();
   });
 });
